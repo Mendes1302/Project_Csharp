cd6183c084b4b5c0314418b11d359115cb105e94
Mendes1302 <lmlucasmendes1302@gmail.com>
2021-09-19 14:38:21 -0300

    Start the project

--- Pylearning/Controllers/HomeController.cs
+++ Pylearning/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Pylearning.Controllers
         {
             return View();
         }
+        public IActionResult Letsgo()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
